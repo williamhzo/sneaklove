1e4c70efec6803f20679b118d83b0c4741d3f5c6
William Hermozo <william.hermozo@protonmail.com>
2020-04-23 11:50:05 +0200

    update user

--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	name: { type: String, required: true },
-	lastname: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+  name: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
