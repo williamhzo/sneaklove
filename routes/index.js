9a8ddb439603c009b9cdae8c7df8b405f37a25d2
TheShua <g.deglisezinski@gmail.com>
2020-04-23 15:05:18 +0200

    Beginning Prod Add logic

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const authentificated = require('../middlewares/authenticated');
 
 router.get('/', (req, res) => {
-  res.render('index.hbs');
+	res.render('index.hbs');
 });
 
+router.get('/prod-add', authentificated, (req, res, next) => {
+	res.render('products_add');
+});
 
+router.post('./prod-add', authentificated, (req, res, next) => {
+	const newSneaker = {
+		name: req.body.name,
+		ref: req.body.ref,
+		sizes: req.body.sizes,
+		description: req.body.description,
+		price: req.body.price,
+		category: req.body.category,
+		id_tags: req.body.tags,
+	};
+
+	console.log(newSneaker);
+});
+
+router.get('/prod-manage', authentificated, (req, res, next) => {
+	res.render('products_manage');
+});
 
 module.exports = router;
