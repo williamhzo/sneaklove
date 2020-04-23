3d73a5c0d4725f81eb27b00dbc1f232f18b14bd6
TheShua <g.deglisezinski@gmail.com>
2020-04-23 11:55:17 +0200

    Adding an S to sneakerS

--- app.js
+++ app.js
@@ -24,16 +24,16 @@ app.use(cookieParser());
 
 // SESSION SETUP
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 60000 }, // in millisec
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60, // 1 day
-    }),
-    saveUninitialized: true,
-    resave: true,
-  })
+	session({
+		secret: process.env.SESSION_SECRET,
+		cookie: { maxAge: 60000 }, // in millisec
+		store: new MongoStore({
+			mongooseConnection: mongoose.connection,
+			ttl: 24 * 60 * 60, // 1 day
+		}),
+		saveUninitialized: true,
+		resave: true,
+	})
 );
 
 app.locals.site_url = process.env.SITE_URL;
@@ -45,27 +45,27 @@ app.locals.site_url = process.env.SITE_URL;
 // WARNING: this function must be declared AFTER the session setup
 // WARNING: this function must be declared BEFORE app.use(router(s))
 function checkloginStatus(req, res, next) {
-  res.locals.user = req.session.currentUser ? req.session.currentUser : null;
-  // access this value @ {{user}} or {{user.prop}} in .hbs
-  res.locals.isLoggedIn = Boolean(req.session.currentUser);
-  // access this value @ {{isLoggedIn}} in .hbs
-  next(); // continue to the requested route
+	res.locals.user = req.session.currentUser ? req.session.currentUser : null;
+	// access this value @ {{user}} or {{user.prop}} in .hbs
+	res.locals.isLoggedIn = Boolean(req.session.currentUser);
+	// access this value @ {{isLoggedIn}} in .hbs
+	next(); // continue to the requested route
 }
 
 function eraseSessionMessage() {
-  var count = 0; // initialize counter in parent scope and use it in inner function
-  return function (req, res, next) {
-    if (req.session.msg) {
-      // only increment if session contains msg
-      if (count) {
-        // if count greater than 0
-        count = 0; // reset counter
-        req.session.msg = null; // reset message
-      }
-      ++count; // increment counter
-    }
-    next(); // continue to the requested route
-  };
+	var count = 0; // initialize counter in parent scope and use it in inner function
+	return function (req, res, next) {
+		if (req.session.msg) {
+			// only increment if session contains msg
+			if (count) {
+				// if count greater than 0
+				count = 0; // reset counter
+				req.session.msg = null; // reset message
+			}
+			++count; // increment counter
+		}
+		next(); // continue to the requested route
+	};
 }
 
 app.use(checkloginStatus);
@@ -74,8 +74,8 @@ app.use(eraseSessionMessage());
 // Getting/Using router(s)
 app.use('/', require('./routes/index'));
 app.use('/', require('./routes/auth'));
-app.use('/sneaker', require('./routes/sneaker'));
+app.use('/sneakers', require('./routes/sneaker'));
 
 const listener = app.listen(process.env.PORT, () => {
-  console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
+	console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
 });
