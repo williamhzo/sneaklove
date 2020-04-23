bc4d45c7599cf9539d227df592e7ebda0065b247
TheShua <g.deglisezinski@gmail.com>
2020-04-23 18:26:19 +0200

    Fix Sneaker route

--- routes/sneaker.js
+++ routes/sneaker.js
@@ -8,63 +8,69 @@ const Tag = require('../models/Tag');
 const Sneaker = require('../models/Sneaker');
 
 router.get('/collection', (req, res, next) => {
-  // Async function to make multiple db queries
-  async function getProducts() {
-    try {
-      // Store all tags in : tags
-      const tags = await Tag.find()
-        .then((dbResult) => dbResult)
-        .catch((dbError) => next(dbError));
-      // Store all sneakers from the database in : sneakers
-      const sneakers = await Sneaker.find()
-        .then((dbResult) => dbResult)
-        .catch((dbError) => next(dbError));
+	// Async function to make multiple db queries
+	async function getProducts() {
+		try {
+			// Store all tags in : tags
+			const tags = await Tag.find()
+				.then()
+				.catch((dbError) => next(dbError));
+			// Store all sneakers from the database in : sneakers
+			const sneakers = await Sneaker.find()
+				.then()
+				.catch((dbError) => next(dbError));
 
-      // Render the products page with all sneakers !
-      res.render('products', {
-        tags,
-        sneakers,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+			// Render the products page with all sneakers !
+			res.render('products', {
+				tags,
+				sneakers,
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  // Execute the async function
-  getProducts();
+	// Execute the async function
+	getProducts();
 });
 
 router.get('/:cat', (req, res) => {
-  const category = req.params.cat;
-  // Async function to make multiple db queries
-  async function getCategory() {
-    try {
-      // Store all tags in : tags
-      const tags = await Tag.find()
-        .then((dbResult) => dbResult)
-        .catch((dbError) => next(dbError));
-      // Store all sneakers from the category in : sneakers
-      const sneakers = await Sneaker.find({ category })
-        .then((dbResult) => dbResult)
-        .catch((dbError) => next(dbError));
+	const category = req.params.cat;
+	// Async function to make multiple db queries
+	async function getCategory() {
+		try {
+			// Store all tags in : tags
+			const tags = await Tag.find()
+				.then((dbResult) => dbResult)
+				.catch((dbError) => next(dbError));
+			// Store all sneakers from the category in : sneakers
+			const sneakers = await Sneaker.find({ category })
+				.then((dbResult) => dbResult)
+				.catch((dbError) => next(dbError));
 
-      // Render the products page of the category
-      res.render('products', {
-        category,
-        tags,
-        sneakers,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+			// Render the products page of the category
+			res.render('products', {
+				category,
+				tags,
+				sneakers,
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  // Execute the async function
-  getCategory();
+	// Execute the async function
+	getCategory();
 });
 
-// router.get('/one-product/:id', (req, res) => {
-//   res.render('one_product.hbs');
-// });
+router.get('/one-product/:id', (req, res) => {
+	Sneaker.findOne({ _id: req.params.id })
+		.then((dbResult) => {
+			res.render('one_product.hbs', {
+				sneaker: dbResult,
+			});
+		})
+		.catch((dbError) => next(dbError));
+});
 
 module.exports = router;
