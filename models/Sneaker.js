83fa8a599c105515c0933c7b84cf5346f86dea86
William Hermozo <william.hermozo@protonmail.com>
2020-04-23 20:02:57 +0200

    add cart features and css styles

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sneakerSchema = new Schema({
-	name: { type: String, required: true },
-	ref: { type: String, required: true },
-	sizes: { type: Number },
-	description: { type: String },
-	price: { type: Number },
-	image: { type: String, default: '/medias/img/blank.jpg' },
-	category: { type: String, enum: ['men', 'women', 'kids'] },
-	id_tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
+  name: { type: String, required: true },
+  ref: { type: String, required: true },
+  sizes: { type: Number },
+  description: { type: String },
+  price: { type: Number },
+  image: { type: String, default: '/medias/img/blank.jpg' },
+  category: { type: String, enum: ['men', 'women', 'kids'] },
+  id_tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
 });
 
 const Sneaker = mongoose.model('Sneaker', sneakerSchema);
