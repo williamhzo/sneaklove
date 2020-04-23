e026e30e3beb761c6659653b7c650135c78fb59c
TheShua <g.deglisezinski@gmail.com>
2020-04-23 11:15:42 +0200

    Add models content

---
+++ models/Sneaker.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sneakerSchema = new Schema({
+	name: { type: String, required: true },
+	ref: { type: String, required: true },
+	sizes: { type: Number },
+	description: { type: String },
+	price: { type: Number },
+	category: { type: String, enum: ['men', 'women', 'kids'] },
+	id_tags: { type: Schema.Types.ObjectId, ref: 'Tag' },
+});
+
+const Sneaker = mongoose.model('Sneaker', sneakerSchema);
+
+module.exports = Sneaker;
